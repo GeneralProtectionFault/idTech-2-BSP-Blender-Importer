3a40382541ef4e529d8111ac9c89c64cc53504e6
GeneralProtectionFault <fleetingmasshysteria@gmail.com>
2025-11-05 22:17:47 -0500

    Version

--- idTech 2 BSP Blender Importer/__init__.py
+++ idTech 2 BSP Blender Importer/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "idTech 2 BSP Importer",
     "author": "GeneralProtectionFault",
-    "version": (1,3,1),
+    "version": (1,3,2),
     "blender": (3,6,0),
     "location": "File > Import > idTech 2 Quake II/Anachronox (.BSP)",
     "warning": "",
